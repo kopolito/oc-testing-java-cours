cf1cec5ad86004f950f21fb3366bac70da363703
kopolito <dev@kopol.xyz>
2020-09-10 14:49:18 +0200

    assertJ

--- src/test/java/com/openclassrooms/testing/CalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/CalculatorTest.java
@@ -1,10 +1,13 @@
 package com.openclassrooms.testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.text.MessageFormat;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -58,7 +61,8 @@ public class CalculatorTest {
 		int somme = calculatorUnderTest.add(a, b);
 
 		// Assert
-		assertEquals(5, somme);
+		// assertEquals(5, somme);
+		assertThat(somme).isEqualTo(5);
 	}
 
 	@Test
@@ -71,7 +75,8 @@ public class CalculatorTest {
 		int produit = calculatorUnderTest.multiply(a, b);
 
 		// Assert
-		assertEquals(462, produit);
+		// assertEquals(462, produit);
+		assertThat(produit).isEqualTo(462);
 	}
 
 	@ParameterizedTest(name = "{0} x 0 doit être égal à 0")
@@ -83,7 +88,8 @@ public class CalculatorTest {
 		int actualResult = calculatorUnderTest.multiply(arg, 0);
 
 		// Assert -- ça vaut toujours zéro !
-		assertEquals(0, actualResult);
+		// assertEquals(0, actualResult);
+		assertThat(actualResult).isEqualTo(0);
 	}
 
 	@ParameterizedTest(name = "{0} + {1} doit être égal à {2}")
@@ -95,7 +101,8 @@ public class CalculatorTest {
 		int actualResult = calculatorUnderTest.add(arg1, arg2);
 
 		// Assert
-		assertEquals(expectResult, actualResult);
+		// assertEquals(expectResult, actualResult);
+		assertThat(actualResult).isEqualTo(expectResult);
 	}
 
 	@Timeout(1)
@@ -108,6 +115,20 @@ public class CalculatorTest {
 
 		// Assert
 		// ...
+	}
+
+	@Test
+	public void digitsSet_shouldReturnsTheSetOfDigits_ofPositiveInteger() {
+		// GIVEN
+		int number = 95897;
+
+		// WHEN
+		Set<Integer> actualDigits = calculatorUnderTest.digitsSet(number);
+
+		// THEN
+		// Set<Integer> expectedDigits = Stream.of(5, 7, 8, 9).collect(Collectors.toSet());
+		// assertEquals(expectedDigits, actualDigits);
+		assertThat(actualDigits).containsExactlyInAnyOrder(5, 7, 8, 9);
 	}
 
 }
